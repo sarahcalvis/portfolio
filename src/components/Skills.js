2d4dcaeb83c7874fcd4aa391eb0e49e053555537
Calvis <sarahcalvis@gmail.com>
2020-03-18 20:37:09 -0400

    Skills section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,21 +1,45 @@
 import React from 'react';
 
+let skills = [
+  'Parallel Programming',
+  'C#',
+  'React',
+  'Vue Native',
+  'Java',
+  'JavaScript',
+  'C/C++',
+  'Python',
+  'HTML/CSS',
+  'Kotlin',
+  'Swift',
+  'SQL',
+  'Firebase',
+  'REST APIs',
+  'Firebase',
+  'Web',
+  'iOS',
+  'Android',
+  'Agile',
+  'Scrum',
+  'Git',
+  'Jira',
+  'Machine Learning',
+  'AI',
+  'Linux'
+]
 export default function Skills() {
+  let skillsString = '';
+
+  for (let skill in skills) {
+    skillsString += skills[skill]
+    skillsString += parseFloat(skill) === parseFloat(skills.length - 1) ? '.' : ', '
+  }
+
   return (
     <div id='section' >
       <div id='vertical-center' >
         <h2>Skills</h2>
-        <div id='horizontal'>
-          <div id='col-padding'>
-            <p>Languages</p>
-          </div>
-          <div id='col-padding'>
-            <p>Libraries</p>
-          </div>
-          <div id='col-padding'>
-            <p>Tools</p>
-          </div>
-        </div>
+        <p>{skillsString}</p>
       </div>
     </div>
   )
