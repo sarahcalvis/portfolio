ce8618eb7e733c57cbc3c34cc00ded2347a0de27
Calvis <sarahcalvis@gmail.com>
2020-03-21 23:04:11 -0400

    Styling fixes

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -41,7 +41,7 @@ let jobs = [
 let experience = jobs.map((job) => {
   return <div id='padding-div'>
     <h4>{job.title}</h4>
-    <h6>{job.company} - {job.location} - {job.duration}</h6>
+    <h5>{job.company} - {job.location} - {job.duration}</h5>
     <p>{job.description}</p>
     <Tags skills={job.skills} />
   </div>
