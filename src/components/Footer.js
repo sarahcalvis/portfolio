ec57b7aa54f1b2e12702bc898b4a22b05ca68cee
Calvis <sarahcalvis@gmail.com>
2020-03-21 22:59:06 -0400

    Footer updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,30 @@
 import React from 'react';
 
+import Email from '../icons/email.svg';
+import LinkedIn from '../icons/linkedin.svg';
+import GitHub from '../icons/github.svg';
+
 export default function Footer() {
   return (
-    <div>
-      <h2>Footer</h2>
-      <p>Headshot</p>
-      <p>Links again</p>
+    <div id='footer'>
+      <p>Sarah Calvis</p>
+      <div id='horizontal'>
+        <div id='col-padding'>
+          <a href='mailto:sarahcalvis@gmail.com'>
+            <img alt="Email" src={Email} id='cover-icon' />
+          </a>
+        </div>
+        <div id='col-padding'>
+          <a href='https://www.github.com/sarahcalvis' target='_blank' rel='noopener noreferrer'>
+            <img alt="GitHub" src={GitHub} id='cover-icon' />
+          </a>
+        </div>
+        <div id='col-padding'>
+          <a href='https://www.linkedin.com/in/sarah-calvis-6aba91150' target='_blank' rel='noopener noreferrer'>
+            <img alt="LinkedIn" src={LinkedIn} id='cover-icon' />
+          </a>
+        </div>
+      </div>
     </div>
   )
 }
